0145263711d4eb4e471b942d90c5eb8163328ac1
goamhobala <yehbebe0112@gmail.com>
2024-09-20 11:46:45 +0200

    got the reversing algorithm working

--- question3/question3.asm
+++ question3/question3.asm
@@ -74,90 +74,133 @@ main:
     syscall
     
     # t0 contains address of 
-    li $v0, 1
-    move $a0, $a1
-    syscall
+    # li $v0, 1
+    # move $a0, $a1
+    # syscall
 
 
     la $s3, 0($a1)
     jal new_line
 
-    li $v0, 1
-    move $a0, $s3
-    syscall
+    # li $v0, 1
+    # move $a0, $s3
+    # syscall
 
     # s0 conatains address of first element
     la $s0, 0($s3)
 
-    jal new_line
-    li $v0, 1
-    move $a0, $s0
-    syscall
+    # jal new_line
+    # li $v0, 1
+    # move $a0, $s0
+    # syscall
 
-    move $a0, $s0
-    li $v0, 4
-    syscall
+    # move $a0, $s0
+    # li $v0, 4
+    # syscall
     
     # s1 contains address of first data
 
 
     la $s1, 44($s3)
 
-    jal new_line
-    li $v0, 1
-    move $a0, $s1
-    syscall
+    # jal new_line
+    # li $v0, 1
+    # move $a0, $s1
+    # syscall
 
     # s2 contains  last data
     sub $t0, $a2, 2
     add $s2, $s0, $t0 
 
-    jal new_line
-    li $v0, 1
-    move $a0, $s2
-    syscall
+    # jal new_line
+    # li $v0, 1
+    # move $a0, $s2
+    # syscall
 
 
 
     sub $t0, $s2, $s1
-    li $t1, 2
-    div  $t0, $t1 #(divide by  2, the number of pairs)
+    li $t1, 4
+    div  $t0, $t1 #(divide by  2 for pairs)
     mfhi $t2
+    
+    # jal new_line
+    # li $v0, 1
+    # move $a0, $t2
+    # syscall
+
     mflo $t3
-    #s3 stores the number of swaps/pairs   
-    add $s3, $t3, $t2 # Add remainder to account for singletons
- 
+
+    # jal new_line
+    # li $v0, 1
+    # move $a0, $t3
+    # syscall
+
+
+
+    #s4 stores the number of swaps/pairs   
+    add $t4, $t3, $t2 # Add remainder to account for singletons
+    addi $s4, $t4, -1 # Subtract 1 cuz we're starting at 0
+    # jal new_line
+    # li $v0, 1
+    # move $a0, $s3
+    # syscall
+
     # lw $t0, 0($s1)
-    # addi $t1, $s2, -2 # $t1 is the last number 
+    # addi $t1, $s2, -2 # $t1 is the last numb    # jal new_line
+    # li $v0, 1
+    # move $a0, $s3
+    # syscaller 
     move $t3, $zero #t3 is the counter
+
+    # jal new_line
+    # li $v0, 1
+    # move $a0, $t3
+    # syscall
 
     jal reverse
 
     li $v0, 10
+
     syscall
 
 
 reverse:
     #t0 address of smaller, t1 address of bigger
     #Calculate index
-    
+      
 
-    beq $t3, $s3, return
+    # li $v0, 1
+    # move $a0, $s4
+    # syscall
+
+    # li $v0, 1
+    # move $a0, $t3
+    # syscall
+
+
+    beq $t3, $s4, return
+    #t0 for head, t1 for tail
     mul $t6, $t3, 2
     add $t0, $t6, $s1
-    sub $t1, $s1, $t6
+    sub $t1, $s2, $t6
     #t2 is a temp to store the value at t0
     
     #testing
-    jal new_line
-    li $v0, 1
-    lh $a0, 0($t0)
-    syscall
+    # jal new_line
+    # li $v0, 1
+    # move $a0, t3
+    # syscall
 
-    jal new_line
-    li $v0, 1
-    lh $a0, 0($t1)
-    syscall
+    # # jal new_line
+    # li $v0, 1
+    # lh $a0, 0($t0)
+    # syscall
+
+    # # jal new_line
+    # li $v0, 1
+    # lh $a0, 0($t1)
+    # syscall
 
 
     lh $t2, 0($t0)
@@ -166,15 +209,25 @@ reverse:
     sh $t4, 0($t0) 
     
         #testing
-    jal new_line
-    li $v0, 1
-    lh $a0, 0($t0)
-    syscall
 
-    jal new_line
-    li $v0, 1
-    lh $a0, 0($t1)
-    syscall
+    # jal new_line
+    # li $v0, 1
+    # lh $a0, 0($t0)
+    # syscall
+
+    # jal new_line
+    # li $v0, 1
+    # lh $a0, 0($t0)
+    # syscall
+    # jal new_line
+    # li $v0, 1
+    # lh $a0, 0($t1)
+    # syscall
+
+    # jal new_line
+    # li $v0, 1
+    # lh $a0, 0($t1)
+    # syscall
 
     addi $t3, $t3, 1
     j reverse
